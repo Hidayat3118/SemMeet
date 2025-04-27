d1cb85fa3f1f1efd971d34e4a60efef972b7c8a3
Hidayat3118 <muhammadhidayat3118@gmail.com>
2025-04-28 07:40:06 +0800

    database fix

--- database/migrations/2025_04_06_055434_create_pesertas_table.php
+++ database/migrations/2025_04_06_055434_create_pesertas_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         
             Schema::create('pesertas', function (Blueprint $table) {
                 $table->id();
-                $table->string('alamat')->nullable();;
+                $table->string('alamat')->nullable();
                 $table->string('foto')->nullable();
+                $table->string('instansi')->nullabel();
                 $table->timestamps();
                 $table->foreignId('user_id')->constrained()->onDelete('cascade');
             });
