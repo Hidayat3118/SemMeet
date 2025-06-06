b3752392f9996c6d55e01012e5991337084a52a0
Hidayat3118 <muhammadhidayat3118@gmail.com>
2025-06-06 22:38:39 +0800

    perbaikan tampilan sertifikat

--- resources/views/sertifikat/pdf.blade.php
+++ resources/views/sertifikat/pdf.blade.php
@@ -1,112 +1,4 @@
-{{-- <!DOCTYPE html>
-<html lang="id">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Sertifikat</title>
-    <style>
-        body {
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-            margin: 0;
-            padding: 0;
-        }
-
-        .sertifikat-container {
-            width: 100%;
-            height: 100%;
-            padding: 40px 60px;
-            box-sizing: border-box;
-            position: relative;
-            background: white;
-            border: 10px solid #2c3e50;
-        }
-
-        .logo {
-            width: 150px;
-        }
-
-        .badge {
-            position: absolute;
-            top: 0;
-            right: 40px;
-            background: #2c3e50;
-            color: white;
-            padding: 20px 10px;
-            writing-mode: vertical-rl;
-            text-align: center;
-            font-weight: bold;
-        }
-
-        .judul {
-            margin-top: 80px;
-            text-align: center;
-            font-size: 32px;
-            font-weight: bold;
-        }
-
-        .nama {
-            margin-top: 50px;
-            text-align: center;
-            font-size: 28px;
-        }
-
-        .deskripsi {
-            margin-top: 30px;
-            text-align: center;
-            font-size: 18px;
-        }
-
-        .ttd {
-            position: absolute;
-            bottom: 60px;
-            right: 60px;
-            text-align: center;
-        }
-
-        .ttd img {
-            height: 50px;
-        }
-
-        .ttd .nama {
-            font-weight: bold;
-            margin-top: 5px;
-        }
-
-        .ttd .jabatan {
-            font-size: 14px;
-            color: gray;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="sertifikat-container">
-        <img src="{{ public_path('logo.png') }}" class="logo" alt="Logo">
-
-        <div class="badge">SERTIFIKAT KELULUSAN</div>
-
-        <div class="judul">SERTIFIKAT</div>
-
-        <div class="deskripsi">Diberikan kepada:</div>
-
-        <div class="nama">{{ $sertifikat->pendaftaran->peserta->user->name }}</div>
-
-        <div class="deskripsi">
-            Atas partisipasi dan kehadiran dalam seminar:<br>
-            <strong>{{ $sertifikat->pendaftaran->seminar->judul }}</strong><br>
-            pada tanggal
-            {{ \Carbon\Carbon::parse($sertifikat->pendaftaran->seminar->waktu)->translatedFormat('d F Y') }}
-        </div>
-
-        <div class="ttd">
-            <img src="{{ public_path('tanda-tangan.png') }}" alt="Tanda Tangan">
-            <div class="nama">{{ $sertifikat->pendaftaran->seminar->pembicara->user->name }}</div>
-            <div class="jabatan">{{ $sertifikat->pendaftaran->seminar->pembicara->instansi }}</div>
-        </div>
-    </div>
-</body>
-
-</html> --}}
 
 <!DOCTYPE html>
 <html lang="id">
@@ -132,15 +24,28 @@
             width: 26cm;
             height: 18cm;
             padding: 40px 50px;
-            border: 15px solid #2c3e50;
+            border: 15px solid #3D90D7;
             margin: auto;
             background-color: #fff;
-
             display: flex;
             flex-direction: column;
             justify-content: space-between;
             position: relative;
 
+        }
+
+        .border-kedua {
+            box-sizing: border-box;
+            height: 590px;
+            padding: 40px 50px;
+            border: 3px solid #3D90D7;
+            margin: auto;
+            background-color: #fff;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            position: relative;
+            border-radius: 4%
         }
 
         .header {
@@ -150,25 +55,26 @@
         }
 
         .logo {
-            width: 150px;
+            width: 90px;
         }
 
         .badge {
             position: absolute;
             top: 30px;
             right: 50px;
-            background-color: #2c3e50;
+            background-color: #3D90D7;
             color: white;
             padding: 10px 20px;
             writing-mode: vertical-rl;
             /* transform: rotate(180deg); */
             font-weight: bold;
-            font-size: 12px;
+            font-size: 15px;
+            border-radius: 4%
         }
 
         .content {
             text-align: center;
-            margin-top: 200px;
+            margin-top: 50px;
         }
 
         .content h1 {
@@ -176,15 +82,12 @@
             margin-bottom: 20px;
         }
 
-        .content p {
-            margin: 4px 0;
-            font-size: 14px;
-        }
 
         .nama {
             font-size: 24px;
             font-weight: bold;
             margin: 30px 0 15px 0;
+            color: #3D90D7
         }
 
         .footer {
@@ -218,48 +121,62 @@
             max-height: 60px;
             margin-bottom: 5px;
         }
+
+        .seminar {
+            font-size: 24px;
+            font-weight: bold;
+            margin: 30px 0 15px 0;
+            color: #3D90D7
+        }
+
+        
     </style>
 </head>
 
 <body>
     <div class="sertifikat-container">
-        <div class="header">
-            <img src="{{ asset('img/icon.jpg') }}" alt="Logo" class="logo">
-        </div>
+        {{-- border kedua --}}
+        <div class="border-kedua">
+            <div class="header">
+                <img src="{{ public_path('img/icon.jpg') }}" alt="Logo" class="logo">
+            </div>
 
-        <div class="badge">SERTIFIKAT KELULUSAN</div>
+            <div class="badge">SERTIFIKAT PARTISIPASI</div>
 
-        <div class="content">
-            <h1>SERTIFIKAT</h1>
-            <p>Diberikan kepada:</p>
-            <div class="nama">{{ $sertifikat->pendaftaran->peserta->user->name }}</div>
-            <p>Atas partisipasi dan kehadiran dalam seminar:</p>
-            <p><strong>{{ $sertifikat->pendaftaran->seminar->judul }}</strong></p>
-            <p>yang diselenggarakan pada tanggal
-                <strong>{{ \Carbon\Carbon::parse($sertifikat->pendaftaran->seminar->waktu)->translatedFormat('d F Y') }}</strong>
-            </p>
-        </div>
+            <div class="content">
+                <h1>SERTIFIKAT</h1>
+                <p class="tes">Diberikan kepada:</p>
+                <div class="nama">{{ $sertifikat->pendaftaran->peserta->user->name }}</div>
+                <p>Atas partisipasi dan kehadiran dalam seminar:</p>
+                {{-- Judul Seminar --}}
+                <p class="seminar">{{ $sertifikat->pendaftaran->seminar->judul }}</p>
+                <p>yang diselenggarakan pada tanggal
+                    <strong>{{ \Carbon\Carbon::parse($sertifikat->pendaftaran->seminar->waktu)->translatedFormat('d F Y') }}</strong>
+                </p>
+            </div>
 
-        <div class="footer">
-            <div class="ttd">
-                {{-- Cek apakah tanda tangan ada --}}
-                @php
-                    $pembicara = $sertifikat->pendaftaran->seminar->pembicara ?? null;
-                @endphp
+            <div class="footer">
+                <div class="ttd">
+                    {{-- Cek apakah tanda tangan ada --}}
+                    @php
+                        $pembicara = $sertifikat->pendaftaran->seminar->pembicara ?? null;
+                    @endphp
 
-                @if ($pembicara && $pembicara->tanda_tangan)
-                    <img src="{{ public_path('storage/' . $pembicara->tanda_tangan) }}" alt="Tanda Tangan">
-                @else
+                    @if ($pembicara && $pembicara->tanda_tangan)
+                        <img src="{{ public_path('storage/' . $pembicara->tanda_tangan) }}" alt="Tanda Tangan">
+                    @else
+                        <div class="garis"></div>
+                    @endif
                     <div class="garis"></div>
-                @endif
-                <div class="garis"></div>
-                <p>{{ $pembicara->user->name ?? 'Nama Pembicara' }}</p>
-                <p><em>Pembicara</em></p>
-                {{-- <div class="garis"></div>
+                    <p>{{ $pembicara->user->name ?? 'Nama Pembicara' }}</p>
+                    <p><em>Pembicara</em></p>
+                    {{-- <div class="garis"></div>
                 <p>{{ $sertifikat->pendaftaran->seminar->pembicara->user->name ?? 'Nama Pembicara' }}</p>
                 <p><em>Pembicara</em></p> --}}
+                </div>
             </div>
         </div>
+
     </div>
 </body>
 
